87f4aed03d0b78304bf0ee952371bc897548ea7a
Andy Hoo <hooa@bc.edu>
2022-12-07 04:41:57 -0500

    Initial Commit

---
+++ iP/iPApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPApp.swift
+//  iP
+//
+//  Created by Andy Hoo on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct iPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
